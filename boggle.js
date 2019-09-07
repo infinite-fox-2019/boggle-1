dac08fd24fa55d9a87bfc0bc842f13c8a3332cda
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-09-07 16:03:00 +0700

    release 0

---
+++ boggle.js
@@ -0,0 +1,41 @@
+class Boggle {
+    constructor() {
+        this.library = this.generateLibrary();
+        this.board = [];
+    }
+
+    shake(side) {
+        let result = [];
+        for(let i = 0; i < side; i++) {
+            result[i] = [];
+            for(let j = 0; j < side; j++) {
+                let randomIndex = Math.floor(Math.random() * Math.floor(this.library.length));
+                result[i].push(this.library[randomIndex]);
+            }
+        }
+
+        this.board = result;
+        return result;
+    }
+
+    generateLibrary() {
+        let result = [];
+        let vowels = ['A', 'E', 'I', 'U', 'O'];
+        for(let i = 0; i < 26; i++) {
+            let letter = String.fromCharCode(65 + i);
+            result.push(letter);
+            if(vowels.includes(letter)) {
+                // for(let j = 0; j < 4; j++) {
+                //     result.push(letter);
+                // }
+                result.push(letter);
+            }
+        }
+        
+        return result;
+    }
+
+}
+
+let game = new Boggle();
+console.log(game.shake(4));
