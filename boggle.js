99c5c1cff0185e55235c3820b45b6994442296dc
aldinofrizal <aldinofrizal@gmail.com>
2019-09-08 02:09:43 +0700

    handle multiple first letter found. increase probability of finding word

--- boggle.js
+++ boggle.js
@@ -9,165 +9,176 @@ class Boggle {
     }
 
     eachSolve(word,board) {
-        let track = []
-
-        track.push(headWord(word, board))
-        if (track[0] === false) return false
-        word = word.slice(1)
-
-
-        for (let i = 0; i < word.length; i++) {
-            let koordinat = track[track.length - 1];
-            if (word[i] === undefined) return false
-            if (checkMove(koordinat, word[i])) {
-                track.push(checkMove(koordinat, word[i]))
-            } else {
-                i -= 2
-                track.push(track[track.length - 2])
-            }
-        }
-
-        return true
-
-        function headWord(word, board) {
-            for (let i = 0; i < word.length; i++) {
-                for (let j = 0; j < board.length; j++) {
-                    // console.log(board[i])
-                    for (let k = 0; k < board[j].length; k++) {
-
-                        if (board[j][k] === word[i]) {
-                            return { row: j, col: k }
-                        }
-                    }
-                }
-            }
-            return false
-        }
-
-        function checkMove(coord, letter) {
-
-            if (utara(coord, letter) && checkTrack(track, utara(coord, letter))) {
-                return utara(coord, letter)
-            } else if (barat(coord, letter) && checkTrack(track, barat(coord, letter))) {
-                return barat(coord, letter)
-            } else if (timur(coord, letter) && checkTrack(track, timur(coord, letter))) {
-                return timur(coord, letter)
-            } else if (selatan(coord, letter) && checkTrack(track, selatan(coord, letter))) {
-                return selatan(coord, letter)
-            } else if (baratDaya(coord, letter) && checkTrack(track, baratDaya(coord, letter))) {
-                return baratDaya(coord, letter)
-            } else if (baratLaut(coord, letter) && checkTrack(track, baratLaut(coord, letter))) {
-                return baratLaut(coord, letter)
-            } else if (timurLaut(coord, letter) && checkTrack(track, timurLaut(coord, letter))) {
-                return timurLaut(coord, letter)
-            } else if (tenggara(coord, letter) && checkTrack(track, tenggara(coord, letter))) {
-                return tenggara(coord, letter)
-            } else return false
-
-
-            function utara(koordinat, letter) {
-                let row = koordinat.row
-                let col = koordinat.col
-                if (row > 0) {
-                    if (board[row - 1][col] === letter) return { row: row - 1, col: col }
-                    else return false
+        
+        let koordinat = headWord(word,board)
+        if(!koordinat.length)return false
+        let status = koordinat.map( x => {
+            return eachSolve(word , board , x)
+        })
+        
+        return status.some( e => {
+            return e === true
+        })
+        
+        function eachSolve(word,board,koordinat) {
+            let track = []
+        
+            track.push(koordinat)
+            if (track[0] === false) return false
+            let words = word.slice(1)
+        
+        
+            for (let i = 0; i < words.length; i++) {
+                let koordinat = track[track.length - 1];
+                if (words[i] === undefined) return false
+                if (checkMove(koordinat, words[i])) {
+                    track.push(checkMove(koordinat, words[i]))
                 } else {
-                    return false
+                    i -= 2
+                    track.push(track[track.length - 2])
                 }
             }
-            function selatan(koordinat, letter) {
-                let row = koordinat.row
-                let col = koordinat.col
-                // console.log(board[row+1][col],letter);
-
-                if (row < board.length - 1) {
-                    if (board[row + 1][col] === letter) return { row: row + 1, col: col }
-                    else return false
-                } else {
-                    return false
-                }
-            }
-            function timur(koordinat, letter) {
-                let row = koordinat.row
-                let col = koordinat.col
-                // console.log(board[row][col+1] , letter);
-
-                if (col < board[0].length - 1) {
-                    if (board[row][col + 1] === letter) return { row: row, col: col + 1 }
-                    else return false
-                } else {
-                    return false
-                }
-            }
-            function barat(koordinat, letter) {
-                let row = koordinat.row
-                let col = koordinat.col
-                if (col > 0) {
-                    if (board[row][col - 1] === letter) return { row: row, col: col - 1 }
-                    else return false
-                } else {
-                    return false
-                }
-            }
-            function baratDaya(koordinat, letter) {
-                let row = koordinat.row
-                let col = koordinat.col
-                if (row < board.length - 1 && col > 0) {
-                    if (board[row + 1][col - 1] === letter) return { row: row + 1, col: col - 1 }
-                    else return false
-                } else {
-                    return false
-                }
-            }
-            function baratLaut(koordinat, letter) {
-                let row = koordinat.row
-                let col = koordinat.col
-                if (row > 0 && col > 0) {
-                    if (board[row - 1][col - 1] === letter) return { row: row - 1, col: col - 1 }
-                    else return false
-                } else {
-                    return false
-                }
-            }
-            function timurLaut(koordinat, letter) {
-                let row = koordinat.row
-                let col = koordinat.col
-                if (row > 0 && col < board.length - 1) {
-                    // console.log(board[row-1][col+1] , letter , row-1,col+1 , row,col );
-
-                    if (board[row - 1][col + 1] === letter) return { row: row - 1, col: col + 1 }
-                    else return false
-                } else {
-                    return false
-                }
-            }
-            function tenggara(koordinat, letter) {
-                let row = koordinat.row
-                let col = koordinat.col
-                // console.log(letter , row-1,col+1 , row,col );
-                if (row < board.length - 1 && col < board.length - 1) {
-
-                    if (board[row + 1][col + 1] === letter) return { row: row + 1, col: col + 1 }
-                    else return false
-                } else {
-                    return false
-                }
-            }
-            function checkTrack(track, koordinat) {
-                let row = koordinat.row
-                let col = koordinat.col
-
-                for (let i = 0; i < track.length; i++) {
-                    if (track[i].row === row && track[i].col === col) {
+        
+            return true
+        
+            function checkMove(coord, letter) {
+        
+                if (utara(coord, letter) && checkTrack(track, utara(coord, letter))) {
+                    return utara(coord, letter)
+                } else if (barat(coord, letter) && checkTrack(track, barat(coord, letter))) {
+                    return barat(coord, letter)
+                } else if (timur(coord, letter) && checkTrack(track, timur(coord, letter))) {
+                    return timur(coord, letter)
+                } else if (selatan(coord, letter) && checkTrack(track, selatan(coord, letter))) {
+                    return selatan(coord, letter)
+                } else if (baratDaya(coord, letter) && checkTrack(track, baratDaya(coord, letter))) {
+                    return baratDaya(coord, letter)
+                } else if (baratLaut(coord, letter) && checkTrack(track, baratLaut(coord, letter))) {
+                    return baratLaut(coord, letter)
+                } else if (timurLaut(coord, letter) && checkTrack(track, timurLaut(coord, letter))) {
+                    return timurLaut(coord, letter)
+                } else if (tenggara(coord, letter) && checkTrack(track, tenggara(coord, letter))) {
+                    return tenggara(coord, letter)
+                } else return false
+        
+        
+                function utara(koordinat, letter) {
+                    let row = koordinat.row
+                    let col = koordinat.col
+                    if (row > 0) {
+                        if (board[row - 1][col] === letter) return { row: row - 1, col: col }
+                        else return false
+                    } else {
                         return false
                     }
                 }
-                return true
+                function selatan(koordinat, letter) {
+                    let row = koordinat.row
+                    let col = koordinat.col
+                    // console.log(board[row+1][col],letter);
+        
+                    if (row < board.length - 1) {
+                        if (board[row + 1][col] === letter) return { row: row + 1, col: col }
+                        else return false
+                    } else {
+                        return false
+                    }
+                }
+                function timur(koordinat, letter) {
+                    let row = koordinat.row
+                    let col = koordinat.col
+                    // console.log(board[row][col+1] , letter);
+        
+                    if (col < board[0].length - 1) {
+                        if (board[row][col + 1] === letter) return { row: row, col: col + 1 }
+                        else return false
+                    } else {
+                        return false
+                    }
+                }
+                function barat(koordinat, letter) {
+                    let row = koordinat.row
+                    let col = koordinat.col
+                    if (col > 0) {
+                        if (board[row][col - 1] === letter) return { row: row, col: col - 1 }
+                        else return false
+                    } else {
+                        return false
+                    }
+                }
+                function baratDaya(koordinat, letter) {
+                    let row = koordinat.row
+                    let col = koordinat.col
+                    if (row < board.length - 1 && col > 0) {
+                        if (board[row + 1][col - 1] === letter) return { row: row + 1, col: col - 1 }
+                        else return false
+                    } else {
+                        return false
+                    }
+                }
+                function baratLaut(koordinat, letter) {
+                    let row = koordinat.row
+                    let col = koordinat.col
+                    if (row > 0 && col > 0) {
+                        if (board[row - 1][col - 1] === letter) return { row: row - 1, col: col - 1 }
+                        else return false
+                    } else {
+                        return false
+                    }
+                }
+                function timurLaut(koordinat, letter) {
+                    let row = koordinat.row
+                    let col = koordinat.col
+                    if (row > 0 && col < board.length - 1) {
+                        // console.log(board[row-1][col+1] , letter , row-1,col+1 , row,col );
+        
+                        if (board[row - 1][col + 1] === letter) return { row: row - 1, col: col + 1 }
+                        else return false
+                    } else {
+                        return false
+                    }
+                }
+                function tenggara(koordinat, letter) {
+                    let row = koordinat.row
+                    let col = koordinat.col
+                    // console.log(letter , row-1,col+1 , row,col );
+                    if (row < board.length - 1 && col < board.length - 1) {
+        
+                        if (board[row + 1][col + 1] === letter) return { row: row + 1, col: col + 1 }
+                        else return false
+                    } else {
+                        return false
+                    }
+                }
+                function checkTrack(track, koordinat) {
+                    let row = koordinat.row
+                    let col = koordinat.col
+        
+                    for (let i = 0; i < track.length; i++) {
+                        if (track[i].row === row && track[i].col === col) {
+                            return false
+                        }
+                    }
+                    return true
+                }
             }
+        
+        
+        
         }
-
-
-
+        
+        function headWord(word, board) {
+            let container = []
+                       
+            for (let i = 0; i < board.length; i++) {
+                for (let j = 0; j < board[i].length; j++) {
+                    if (board[i][j] === word[0])  container.push( { row : i , col : j} )
+                }
+            }
+        
+            if(container.length > 0)return container
+            else return false
+        }
     }
 
     board() {
@@ -194,7 +205,7 @@ class Boggle {
         let words = []
         for (let i = 0; i < library.length; i++) {
             if (this.eachSolve(library[i], board)) {
-                words.push(library[i])
+                words.push( library[i] )
                 counter++
             }
         }
@@ -202,5 +213,8 @@ class Boggle {
     }
 }
 
+
 let game = new Boggle(library)
 console.log(game.solve())
+
+
