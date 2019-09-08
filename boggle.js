219f3ce3da138b5caeb4a4ea38ba13eba0f1dcad
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-09-08 16:56:25 +0700

    hour-1

--- boggle.js
+++ boggle.js
@@ -3,117 +3,127 @@ const data = require('./data');
 class Boogle {
     constructor (data) {
         this.list = data;
-        this.realKamus = this.pecahKamus();
-        this.games = this.printBoard();
+        // this.kamus = this.printKamus();
+        this.games = [];
+        this.resultt = [];
     }
-
-    randWords () {
-        let book = 'AAAAAABBCCDDEEEEEEFFGGHHIIIIIJJKKLLMMNNOOOOOOPPQQRRSSTTUUUUUUVVWWXXYYZZ';
-        let cetak = [];
-        let i = 0;
-        while(i<16){
-            let j = Math.floor(Math.random() * 71);
-            cetak.push(book[j]);
-            i++;
-        }
-        return cetak;
-    }
-    printKamus () {
-        let dasar = []
-        for(let i=0; i<this.list.length; i++){
-            dasar.push(this.list[i]);
-        }
-        return dasar;
-    }
-    pecahKamus () {
-        let awal = this.printKamus();
-        let count = 0;
-        let result = []
-        for(let i=0; i<awal.length; i++){
-            result.push([])
-            for(let j=0; j<awal[i].length; j++){
-                result[count].push(awal[i][j])
-            }
-            count++;
-        }
-        return result;
-    }
-    printBoard(){
-        let huruf = this.randWords();
-        // let cetak = [];    
-        let papan = []
-        let count = 0;
+    // printKamus () {
+    //     let dasar = []
+    //     for(let i=0; i<this.list.length; i++){
+    //         // let worddd = this.list;
+    //         // let randomm = Math.floor(Math.random() * (worddd.length));
+    //         // let so = worddd[randomm]
+    //         dasar.push(this.list[i]);
+    //     }
+    //     // let dasar = ['DOG','FAT','CAR','DO','FIT','AS','BA','CS']
+    //     return dasar;
+    // }
+    shake () {
+        // let result = []
+        //[['D','O','A','V'],['G','G','G','S'],['D','T','A','F'],['E','O','T','A'],];
         for(let i=0; i<4; i++){
-            papan.push([]);
+            this.games.push([]);
             for(let j=0; j<4; j++){
-                papan[i].push(huruf[count]);
-                count++;
+                let kamus = 'AAAAAAAAAABBCCDDEEEEEEEEEEEFFGGHHIIIIIIIIIIJJKKLLMMNNOOOOOOPPQQRRSSTTUUUUUUVVWWXXYYZZ';
+                let random = Math.floor(Math.random() * (kamus.length))
+                let hasRand = kamus[random];
+                this.games[i].push(hasRand);
             }
         }
-        return papan;
+        return this.games;
     }
-    check () {
-        let kams = this.realKamus;
-        let bord = this.games;
-        let index = [];
-        let tamp = ''
-        let k = 0;
-        while(k)
-        for(let i=0; i<bord.length; i++) {
-            for(let j=0; j<bord[i].length; j++) {
-                if(this.checkPos( kams, i , j )) {
-                    tamp += bord[i][j];
-                    break;
+    // takeIndex ( tempI ,tempJ ) {
+    //     let nameWay = [];
+    //     let indexi = tempI;
+    //     let indexj = tempJ;
+    //     let i = 0;
+    //     while( i < 8 ) {
+    //         indexi += this.arah[i].i;
+    //         indexj += this.arah[i].j;
+    //         if(this.games[indexi] === undefined || this.games[indexj] === undefined ) {
+    //             indexi = tempI;
+    //             indexj = tempJ;
+    //             i++;
+    //         }
+    //         else{
+    //             nameWay.push({i : this.arah[indexi].i, j : this.arah[indexj].j });
+    //             indexi = tempI;
+    //             indexj = tempJ;
+    //             i++
+    //         }
+    //     }
+    //     return nameWay;
+    // }
+    finder () {
+        this.shake();
+        console.table(this.games)
+        for( let i=0; i<this.games.length; i++ ) {
+            for( let j=0; j<this.games[i].length; j++ ) {
+                for( let k=0; k<this.list.length; k++ ) {
+                    if( this.games[i][j] === this.list[k][0] ) {
+                        this.solve( i , j , this.list[k] );
+                    }
                 }
             }
         }
-        return tamp;
+        // console.log(this.games);
+        console.log(`ada ${this.resultt.length} kata : \n ${this.resultt}`);
     }
-    checkPos ( kms, i , j ) {
-        // for(let i=0; i<brd.length; i++) {
-            // for(let j=0; j<brd[i].length; j++) {
-                let check = false;
-                if( i === 0 && j === 0 ) { //check kanan 2x2 ;
-                    if( this.games[i][j+1] === kms[0] || this.games[i+1][j+1] === kms[0] || this.games[i+1][j] === kms[0] ) check = true;
+    solve (tempI , tempJ , word) {
+        let arah = [
+            { name : 'atas', i : -1, j : 0 },
+            { name : 'atas-kanan', i : -1, j : 1 },
+            { name : 'kanan', i : 0, j : 1 },
+            { name : 'bawah-kanan', i : 1, j : 1 },
+            { name : 'bawah', i : 1, j : 0 },
+            { name : 'bawah-kiri', i : 1, j : -1 },
+            { name : 'kiri', i : 0, j : -1 },
+            { name : 'kiri-atas', i : -1, j : -1 }
+        ];
+        let flag = true;
+        let count = 1; //pencarian selanjutnya index word ke 1;
+        let histories = [];
+
+        while ( flag ) {
+            let found = false;
+            for( let i=0; i<arah.length; i++ ) {
+                let newtempI = tempI + arah[i].i;
+                let newtempJ = tempJ + arah[i].j;
+
+                if( newtempI >= 0 && newtempI < this.games.length && newtempJ >= 0 && newtempJ < this.games.length ) {
+                    if( this.games[newtempI][newtempJ] === word[count] ) {
+                        histories.push({i : tempI, j : tempJ, value : this.games[tempI][tempJ]});
+                        tempJ = newtempJ;
+                        tempI = newtempI;
+                        count++;
+                        found = true;
+                        this.games[tempI][tempJ] = ' ';
+                        break;
+                    }
                 }
-                if( i === 0 && j === 3 ) { // check kiri 2x2 ;
-                    if( this.games[i][j-1] === kms[0] || this.games[i+1][j-1] === kms[0] || this.games[i+1][j] === kms[0] ) check = true;
-                }
-                if( i === 3 && j === 0 ) { // check kanan atas 2x2 ;
-                    if( this.games[i-1][j] === kms[0] || this.games[i-1][j+1] === kms[0] || this.games[i][j+1] === kms[0] ) check = true;
-                }
-                if( i === 3 && j === 3 ) { // check kiri atas 2x2 ;
-                    if( this.games[i][j-1] === kms[0] || this.games[i-1][j-1] === kms[0] || this.games[i-1][j] === kms[0] ) check = true;
-                }
-                if( i === 0 && j === 1 || j === 2 ) { //check kiri, kanan, bawah ;
-                    if( this.games[i][j-1] === kms[0] || this.games[i][j+1] === kms[0] || this.games[i+1][j-1] === kms[0] || this.games[i+1][j] === kms[0] || this.games[i+1][j+1] === kms[0] ) check = true;
-                }
-                if( i === 3 && j === 1 || j === 2 ) { //check kiri, kanan, atas ;
-                    if( this.games[i-1][j-1] === kms[0] || this.games[i-1][j+1] === kms[0] || this.games[i][j+1] === kms[0] || this.games[i][j-1] === kms[0] || this.games[i][j] === kms[0] ) check = true;
-                }
-                if( i === 1 || i === 2 && j === 0 ) { //check atas, bawah, kanan ;
-                    if( this.games[i-1][j] === kms[0] ||this.games[i+1][j] === kms[0] ||this.games[i][j+1] === kms[0] ||this.games[i-1][j+1] === kms[0] ||this.games[i+1][j+1] === kms[0] ) check = true;
-                }
-                if( i === 1 || i === 2 && j ===3 ) { //check atas, bawah, kiri ;
-                    if( this.games[i-1][j] === kms[0] ||this.games[i+1][j] === kms[0] ||this.games[i][j-1] === kms[0] ||this.games[i-1][j-1] === kms[0] ||this.games[i+1][j-1] === kms[0] ) check = true;
-                }
-                if( i === 1 && j === 1 || i === 1 && j === 2 || i === 2 && j === 1 || i === 2 && j ===2 ) {
-                    if( this.games [i][j-1] === kms[0] ||this.games [i][j+1] === kms[0] ||this.games [i-1][j] === kms[0] ||this.games [i+1][j] === kms[0] ||this.games [i-1][j-1] === kms[0] ||this.games [i+1][j+1] === kms[0] ||this.games [i+1][j-1] === kms[0] ||this.games [i-1][j+1] === kms[0] ) check = true;
-                }
-                if( check ) {
-                    return [i,j]
-                }else {
-                    return 'tidak menemukan apa2 silakan coba lagi';
-                }
-            // }
-        // }
+            }
+            if( !found ) {
+                flag = false;
+            }
+            if( count === word.length ) {
+                flag = false;
+                this.resultt.push(word);
+            }
+        }
+        for( let i=0; i< histories.length; i++ ) {
+            this.games[histories[i].i][histories[i].j] = histories[i].value ;
+            break;
+        }
     }
 }
 
 const game = new Boogle (data);
 // console.log(game.printKamus());
 // console.log(game.pecahKamus());
-console.log(game.realKamus)
-console.log(game.games)
-console.log(game.check());
-// console.log(game)
+// console.log(game.realKamus)
+// console.log(game.solve())
+// console.log(game.check());
+// console.log(game.kamus);
+// console.log(game.checkSaja('DOG'))
+game.finder();
+// console.log(game.checkSaja())
